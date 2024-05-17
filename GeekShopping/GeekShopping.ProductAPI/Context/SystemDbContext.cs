f8c0558160cf378e2dc0e9022bad47e962d60b9e
Joao Henrique <joaoadsistemas@gmail.com>
2024-05-17 16:52:27 -0300

    initial setup

---
+++ GeekShopping/GeekShopping.ProductAPI/Context/SystemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.ProductAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Context
+{
+    public class SystemDbContext : DbContext
+    {
+
+        public SystemDbContext(DbContextOptions<SystemDbContext> options) : base(options) { }
+        
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
